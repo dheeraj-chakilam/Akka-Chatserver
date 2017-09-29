ee96df8fa6e57f3e85ed47651d65f1999d3a796a
Dheeraj Chakilam <dc655@cornell.edu>
2017-09-29 18:27:37 -0400

    Working with F# Akka except for stress test case

---
+++ AkkaMUD/Main.fs
@@ -0,0 +1,34 @@
+﻿module AkkaMUD.Main
+
+open Akka.FSharp
+open World
+open Network
+
+let beatrate = 100.
+
+let config =
+    (Akka.Configuration.ConfigurationFactory.ParseString
+        @"
+        akka {
+        suppress-json-serializer-warning = on
+        }").WithFallback(Configuration.defaultConfig())
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [|id; n; port|] ->
+        let system = System.create "system" config
+        let roomRef = spawn system "room" room
+        let serverRef = spawn system "server" (server roomRef ChatServer (20000 + int id) (int id) (int n))
+        let mServerRef = spawn system "master-server" (server roomRef MasterServer (int port) (int id) (int n))
+        
+        system.Scheduler.ScheduleTellRepeatedly(System.TimeSpan.FromMilliseconds <| 0.,
+                                                    System.TimeSpan.FromMilliseconds <| beatrate,
+                                                    roomRef,
+                                                    SendHeartbeat id)
+
+    | _ -> printfn "Incorrect arguments (%A)" argv
+    
+    while true do
+        ()
+    0
