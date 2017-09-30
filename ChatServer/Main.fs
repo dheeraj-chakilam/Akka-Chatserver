42c308a63616378535f6c1833867695cfe62c199
Dheeraj Chakilam <dc655@cornell.edu>
2017-09-29 23:52:31 -0400

    Renamed solution to ChatServer

---
+++ ChatServer/Main.fs
@@ -0,0 +1,31 @@
+﻿module ChatServer.Main
+
+open Akka.FSharp
+open World
+open Network
+
+let beatrate = 100.
+let aliveThreshold = 1000L
+
+let config =
+    (Akka.Configuration.ConfigurationFactory.ParseString
+        @"
+        akka {
+        suppress-json-serializer-warning = on
+        }").WithFallback(Configuration.defaultConfig())
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [|id; n; port|] ->
+        let system = System.create "system" config
+        let roomRef = spawn system "room" (room id beatrate aliveThreshold)
+        let serverRef = spawn system "server" (server roomRef ChatServer (20000 + int id) (int id) (int n))
+        let mServerRef = spawn system "master-server" (server roomRef MasterServer (int port) (int id) (int n))
+        ()
+
+    | _ -> printfn "Incorrect arguments (%A)" argv
+    
+    while true do
+        ()
+    0
